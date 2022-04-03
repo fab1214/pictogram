79da674e57c5e2ce631baa0564e9c9102edd2ac8
fab1214 <88295561+fab1214@users.noreply.github.com>
2022-04-03 12:43:46 -0400

    fix logged out user post information

--- src/App.js
+++ src/App.js
@@ -204,7 +204,7 @@ function App() {
         <Post
         key={id}
         postId={id}
-        username={post.displayName}
+        username={post.username}
         caption={post.caption}
         imageUrl={post.imageUrl}
         user={user}
